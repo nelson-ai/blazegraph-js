c43c0232166acf2db321ecec40bd206d30c4e491
Viliam Simko <viliam.simko@gmail.com>
2021-12-28 02:05:14 +0100

    refactor: remove "compose" from SPARQL function - reimplemented using builder pattern rather than ramda's compose

--- src/sparql.ts
+++ src/sparql.ts
@@ -1,31 +1,22 @@
-import {compose, flatten, join, replace, zip} from "ramda";
-
-// simple helper functions
-
-const removeLeadingSpaces = replace(/^([\n ])+/g, "")
-const removeTrailingSpaces = replace(/\n+ +/g, "\n")
-const removeFinalNewline = replace(/\n$/, "")
-
-// Public API
+import {zip} from "ramda";
 
 /**
- * String interpolation function that formats
- * SPARQL code by removing leading spaces from each line.
+ * Formats SPARQL code by removing leading spaces from each line.
+ *
  * This function is pure.
  * @author Viliam Simko
+ *
  * @example
  *   const q = SPARQL`
  *     select * from {?s ?p ?o}
  *   `
  *   console.log(q) // -> "select * from {?s ?p ?o}"
  */
-export const SPARQL = (str: TemplateStringsArray, ...vars: string[]): string =>
-  compose(
-    removeFinalNewline,
-    removeTrailingSpaces,
-    removeLeadingSpaces,
-    join(""), // now just a string
-    flatten, // now merged into a single array
-    zip(str), // str and vars are arrays
-    arr => [...arr, ""]
-  )(vars)
+export const SPARQL = (str: TemplateStringsArray, ...vars: string[]): string => {
+  return zip(str, [...vars, ""])
+    .flat()
+    .join("")
+    .replace(/^([\n ])+/g, "") // leading space
+    .replace(/\n+ +/g, "\n") // trailing space
+    .replace(/\n$/, ""); // final newline
+}
